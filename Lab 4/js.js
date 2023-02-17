e06ce961c671b44b6140013cce2e28773aa89243
Alan <A01708668@tec.com>
2023-02-17 12:53:22 -0600

    Añadir botón que enseñe y oculte respuestas dek lab 4

--- Lab 4/js.js
+++ Lab 4/js.js
@@ -1,102 +1,30 @@
-// Se puede usar la palabra reservada VAR para declarar variables
-// Pero usaremos LET el cual hace lo mismo pero con mayor alcance en las variabkes
-
-var comida = "chilaquiles";
-
-let cena = "tacos";
-
-const precio = 70;
-
-console.log("hola");
-console.info("Valor de la comida: " + comida);
-console.warn("El precio no se puede modificar");
-console.error("Te dije que el precio no se puede modificar");
-
-// En js el '=' representa asignación (como en todos los demás lenguajes) 
-// El '==' es una comparación de valor
-// El '===' es una comparación de valor y tipo
-console.assert(1 === 1);
-console.assert(1 === "1");
-console.assert(1 == "1");
-console.assert(1 == true);
-console.assert(1 === true);
-
-// Alancance de variables. SIEMPRE USA LET (a menos que sepas que haces y tengas razones para usar VAR)
-// Con let el alcance es en función 
-// Con var el alcance es global
-for(var i = 1; i<=10; i++){
-    console.log(i);
-}
-console.log(i);
-
-// alert, prompt, confirm
-alert("hola!");
-
-let nombre = prompt("¿Cómo te llamas?");
-
-console.log("Hola " + nombre);
-
-let is_hungry = confirm("¿Tienes habmre?");
-
-console.log(is_hungry);
-
-
-
-// funciones tradicionales
-function numero_tacos () {
-    return 5;
-}
-
-console.log(numero_tacos());
-
-
-// funciones modernas
-let cantidad_tacos = () => {return 5;}
-
-console.log(cantidad_tacos());
-
-// html  dinámico con eventos
 let boton = document.getElementById("respuestas");
 boton.innerHTML = "Enseñar Respuestas";
+let textoVisible = true;
 boton.onclick = () => {
-    document.getElementById("preguntas_lab4").innerHTML =
-    "Las respuestas"    
+    let texto = `
+    <ul>
+        <li><b>¿Qué diferencias y semejanzas hay entre Java y JavaScript?</b></li>
+        Resp
+        <li><b>¿Qué métodos tiene el objeto Date? (Menciona al menos 5*)</b></li>
+        Resp
+        <li><b>¿Qué métodos tiene el objeto Date? (Menciona al menos 5*)</b></li>
+        Resp
+        <li><b>¿Qué métodos tiene el objeto Date? (Menciona al menos 5*)</b></li>
+        Resp
+        <li><b>¿Qué métodos tiene el objeto Date? (Menciona al menos 5*)</b></li>
+        Resp
+    </ul>          
+  `;
+
+  let elemento = document.getElementById("preguntas_lab4");
+
+  if (textoVisible) {
+    elemento.innerHTML = texto;
+    boton.innerHTML = "Ocultar Respuestas";
+  } else {
+    elemento.innerHTML = "";
+    boton.innerHTML = "Enseñar Respuestas";
+  }
+  textoVisible = !textoVisible; // Cambia el estado de la variable
 }
-
-// arreglos
-const arreglo = ["Elemento"];
-
-// Se pueden añadir cosas al array
-arreglo.push("Otro Elemento");
-arreglo["dos"] = 2;
-arreglo.length = 10;
-arreglo[20] = "fin del arreglo";
-
-console.log(arreglo);
-
-for (let elemento in arreglo) {
-    console.log(elemento);
-}
-
-for (let valor of arreglo) {
-    console.log(valor);
-}
-
-// Objetos
-const objeto = {
-    atributo_1: "valor 1",
-    atributo_2: 2
-}
-
-console.log(objeto.atributo_1);
-console.log(objeto.atributo_2);
-objeto.atributo_3 = "tres";
-
-console.log(objeto);
-
-for(let elemento in objeto) {
-    console.log(elemento);
-}
-
-const arreglo_de_objetos = [{atributo_1: "valor 0"}, {atributo_2: 1}, objeto];
-console.log(arreglo_de_objetos);
