958c56714ad94be1b367837332f3681121a89dc5
Alan <A01708668@tec.com>
2023-02-19 20:24:59 -0600

    Añadir ejercicio 4 y 5

--- Lab 4/js.js
+++ Lab 4/js.js
@@ -8,7 +8,7 @@ for(let i = 1; i <= num; i++) {
 }
 
 // Ejercicio 2:
-let num1 =  Math.floor(Math.random() * 10);
+let num1 =  Math.floor(Math.random() * 20);
 let num2 =  Math.floor(Math.random() * 10);
 const resultado = num1 + num2;
 const tiempoInicio = Date.now();
@@ -40,7 +40,8 @@ function contador (array) {
       positivos++;
     }
   }
-  ej3.innerHTML += `Números negativos: ${negativos}<br>
+  ej3.innerHTML += `<br>${array}<br>
+  Números negativos: ${negativos}<br>
   Número de 0's: ${ceros}<br>
   Número de positivos: ${positivos}<br>`;
   return [negativos, ceros, positivos];
@@ -50,6 +51,41 @@ console.assert(JSON.stringify(contador([1, 2, 0, -1, -2])) === JSON.stringify([2
 console.assert(JSON.stringify(contador([0, 0, 0])) === JSON.stringify([0, 3, 0]));
 
 // Ejercicio 4:
+function promedios(matriz) {
+  const promedios = [];
+  let ej4 = document.getElementById("ej4");
+  for (let i = 0; i < matriz.length; i++) {
+    let suma = 0;
+    for (let j = 0; j < matriz[i].length; j++) {
+      suma += matriz[i][j];
+    }
+    const promedio = suma / matriz[i].length;
+    promedios.push(promedio);
+  }
+  ej4.innerHTML += `<br>${matriz}<br>
+    Los promedios son: ${promedios}<br>`;
+  return promedios;
+}
+// Prueba
+console.assert(JSON.stringify(promedios([[1, 2, 3], [4, 5, 6], [7, 8, 9]])) === JSON.stringify([2, 5, 8]));
+console.assert(JSON.stringify(promedios([[2, 4, 6], [1, 3, 5]])) === JSON.stringify([4, 3]));
+
+// Ejercicio 5:
+function inverso(num) {
+  const str_num = num.toString();
+  let resultado = "";
+  let ej5 = document.getElementById("ej5");
+  for (let i = str_num.length - 1; i >= 0; i--) {
+    resultado += str_num[i];
+  }
+  ej5.innerHTML += `<br>${num}<br>
+    En inverso: ${resultado}<br>`;
+  return (resultado);
+}
+
+// Prueba
+console.assert(inverso(Math.floor(Math.random() * 100000)));
+console.assert(inverso(Math.floor(Math.random() * 100000)))
 
 
 // Botón
