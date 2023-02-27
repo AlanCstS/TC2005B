e21c306df430a32e350b487c76d5cc9f44ca0468
Alan <A01708668@tec.com>
2023-02-27 16:56:22 -0600

    Función escribir string con fs

--- Lab 8/app.js
+++ Lab 8/app.js
@@ -3,10 +3,10 @@ console.info("Este script se está ejecutando desde una computadora y no desde e
 console.warn("document, alert, confirm y prompt, no existen en el entorno de ejecución de node");
 console.error("Así se ve un error");
 
-//fs es el módulo para trabajar con el sistema de archivos
-const filesystem = require('fs');
-filesystem.writeFileSync('hola.txt', 'Hola desde node');
-console.log("Terminamos de escribir el archivo");
+// //fs es el módulo para trabajar con el sistema de archivos
+// const filesystem = require('fs');
+// filesystem.writeFileSync('hola.txt', 'Hola desde node');
+// console.log("Terminamos de escribir el archivo");
 
 const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
 
@@ -40,3 +40,11 @@ function promedio(arreglo){
 
 let res = promedio(arreglo);
 console.log("El promedio es: " + res);
+
+function escribirString(str){
+    const filesystem = require('fs');
+    filesystem.writeFileSync('string.txt', `${str}`);
+}
+
+escribirString("Esto es parte de una función del laboratorio 8");
+console.log("Archivo de texto con string 'Esto es parte de una función del laboratorio 8' creado exitosamente!");
