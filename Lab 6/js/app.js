77ff0f8d9b05e22560890678bb7969c770bd730e
Alan <A01708668@tec.com>
2023-03-06 12:40:00 -0600

    Agregar Lab 6 terminado

--- Lab 6/js/app.js
+++ Lab 6/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+
+let boton = document.getElementById("btn");
+boton.onclick = () => {
+    const pass1 = document.getElementById("pass1").value;
+    const pass2 = document.getElementById("pass2").value;
+
+    if(pass1 === pass2 && pass1){
+        alert("Iniciando sesión")
+    } else {
+        alert("Las contraseñas no coinciden")
+    }
+}
