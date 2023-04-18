6f6ce63de563a8618df76929c981ea53ab778b91
Alan <A01708668@tec.com>
2023-04-18 17:15:33 -0600

    Usar has-create en las ruteas

--- Lab 19/routes/formula1.routes.js
+++ Lab 19/routes/formula1.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+const hasCreate = require('../util/has-create');
+
 const pilotosController = require('../controllers/pilotos.controller');
 
-router.get('/nuevo', pilotosController.get_nuevo);
+router.get('/nuevo', hasCreate, pilotosController.get_nuevo);
 
-router.post('/nuevo', pilotosController.post_nuevo);
+router.post('/nuevo', hasCreate, pilotosController.post_nuevo);
 
 router.get('/:id', pilotosController.listar);
 
